5eaa04d284b49d3e950effde6b059c3928ad7719
Dir-A <g2576612205@gmail.com>
2024-09-03 15:24:16 +0800

    bug fix

--- src/src/ZxJson/JIO.cpp
+++ src/src/ZxJson/JIO.cpp
@@ -5,7 +5,7 @@
 
 namespace ZQF::ZxJson
 {
-    auto LoadViaMemory(std::span<const char> spData) -> JValue
+    auto LoadViaMemory(const std::span<const char> spData) -> JValue
     {
         JValue value;
         JParser{ spData }.Parse(value);
@@ -18,14 +18,14 @@ namespace ZQF::ZxJson
         return ZxJson::LoadViaMemory(std::span{ file_data.get(), file_size });
     }
 
-    auto StoreViaMemory(JValue& rfJValue, bool isFormat) -> std::string
+    auto StoreViaMemory(const JValue& rfJValue, const bool isFormat) -> std::string
     {
         std::string dump_str;
         rfJValue.Dump(dump_str, isFormat, 0);
         return dump_str;
     }
 
-    auto StoreViaFile(const std::string_view msPath, JValue& rfJValue, bool isFormat, bool isForceSave) -> void
+    auto StoreViaFile(const std::string_view msPath, const JValue& rfJValue, const bool isFormat, const bool isForceSave) -> void
     {
         auto dump_str = ZxJson::StoreViaMemory(rfJValue, isFormat);
         Private::WriteAllBytes(msPath, dump_str, isForceSave);
--- src/src/ZxJson/JIO.h
+++ src/src/ZxJson/JIO.h
@@ -6,8 +6,8 @@
 
 namespace ZQF::ZxJson
 {
-    auto LoadViaMemory(std::span<const char> spData) -> JValue;
+    auto LoadViaMemory(const std::span<const char> spData) -> JValue;
     auto LoadViaFile(const std::string_view msPath) -> JValue;
-    auto StoreViaMemory(JValue& rfJValue, bool isFormat) -> std::string;
-    auto StoreViaFile(const std::string_view msPath, JValue& rfJValue, bool isFormat, bool isForceSave) -> void;
+    auto StoreViaMemory(const JValue& rfJValue, const bool isFormat) -> std::string;
+    auto StoreViaFile(const std::string_view msPath, const JValue& rfJValue, const bool isFormat, const bool isForceSave) -> void;
 } // namespace ZQF::ZxJson
